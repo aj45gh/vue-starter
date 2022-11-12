b7f97185720233c49b4fef279d34d4aced012586
Alex J <alexjames12345@hotmail.com>
2022-11-12 15:54:02 -0500

    added example of separate file routing

---
+++ src/components/ProductSummary.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="products">
+        Hi
+    </div>
+</template>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import ProductSummary from '../components/ProductSummary.vue'
+</script>
+
+<template>
+    <main>
+        <ProductSummary />
+    </main>
+</template>
